5a089e2c5045988c82bdcae8368b719a863a177c
Nikolacv0 <nikolacvetkovic19092007@gmail.com>
2026-04-22 16:34:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
